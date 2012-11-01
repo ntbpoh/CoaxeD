abb21b8a14fac21d6bab029b6cb09fe591aad7a3
Toby <ntbpoh@gmail.com>
2012-11-01 11:59:30 +0000

    CoaxeD V0.0 - Source to help coax my D dawbings into compilable life. Initial foray into authoring software which works on both Windows and Linux using the D language and the Gtk+3 (GtkD) framework. Most of this source has been pinched from existing examples. The project is only useful as a kind of scratchpad for developing working code to be used in other projects.

---
+++ CoaxeD/Env.bat
@@ -0,0 +1,1 @@
+set LIB=C:\D\dmd2\windows\lib;C:\Users\Public\Public Development\GtkD-2.0\lib\
---
+++ CoaxeD/Lk.bat
@@ -0,0 +1,2 @@
+rem set LIB=%LIB%;C:\D\dmd2\windows\lib;C:\Users\Public\Public Development\GtkD-2.0\lib\
+Link @CoaxeD.lnkarg
